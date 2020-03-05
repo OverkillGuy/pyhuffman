db7742610ed77b9f8ebf2a64ccb0f6b27cb4c18e
Jb DOYON <jb@jiby.tech>
2020-03-05 23:29:52 +0000

    Add basic huffman types
    Add dummy function using the types

--- pyhuffman/huffman.py
+++ pyhuffman/huffman.py
@@ -1,4 +1,15 @@
+from typing import NewType, Tuple, Sequence
 
 
-def some_func():
+# A binary symbol to encode
+Symbol = NewType("Symbol", str)
+
+# The probability of an event
+Probability = NewType("Probability", float)
+
+# A pair of symbol and its probability of occurence
+SymbolProbabilityPair = Tuple[Symbol, Probability]
+
+
+def huffman_table(symbols_probas: Sequence[SymbolProbabilityPair]) -> int:
     return 2
--- tests/test_pyhuffman.py
+++ tests/test_pyhuffman.py
@@ -1,5 +1,13 @@
 import pytest
 from pyhuffman import huffman
 
-def test_func_ok():
-    assert huffman.some_func() == 2, "Bad funccall"
+# Scenario: Showcase the strong typing
+def test_simple_huffman():
+    # Given a string symbol "1001" and probability 0.2
+    symbol = huffman.Symbol("1001")
+    probability = huffman.Probability(0.2)
+    symbol_proba = tuple([symbol, probability])
+    # When building a huffman table
+    table = huffman.huffman_table(list(symbol_proba))
+    # Then table makes sense
+    assert table == 2, "Didn't even succeed at building a dumb table"
