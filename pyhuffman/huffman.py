d97092cc88c7348a3d7e58100d3daedd62cc0364
Jb DOYON <jb@jiby.tech>
2020-03-05 23:37:31 +0000

    Refactor symbols to dict

--- pyhuffman/huffman.py
+++ pyhuffman/huffman.py
@@ -1,15 +1,8 @@
-from typing import NewType, Tuple, Sequence
+from typing import Dict
+
+# Pairs of symbol and their probability of occurence
+SymbolTable = Dict[str, float]
 
 
-# A binary symbol to encode
-Symbol = NewType("Symbol", str)
-
-# The probability of an event
-Probability = NewType("Probability", float)
-
-# A pair of symbol and its probability of occurence
-SymbolProbabilityPair = Tuple[Symbol, Probability]
-
-
-def huffman_table(symbols_probas: Sequence[SymbolProbabilityPair]) -> int:
+def huffman_table(symbols_probas: SymbolTable) -> int:
     return 2
--- tests/test_pyhuffman.py
+++ tests/test_pyhuffman.py
@@ -1,13 +1,18 @@
 import pytest
-from pyhuffman import huffman
+from pyhuffman.huffman import huffman_table, SymbolTable
 
 # Scenario: Showcase the strong typing
 def test_simple_huffman():
     # Given a string symbol "1001" and probability 0.2
-    symbol = huffman.Symbol("1001")
-    probability = huffman.Probability(0.2)
-    symbol_proba = tuple([symbol, probability])
+    symbol_table = {"1001": 0.2}
     # When building a huffman table
-    table = huffman.huffman_table(list(symbol_proba))
+    table = huffman_table(symbol_table)
     # Then table makes sense
     assert table == 2, "Didn't even succeed at building a dumb table"
+
+
+# Scenario: Sample huffman table building
+def test_example_huffman_table():
+    """ From MIT course example"""
+    # Given a symbol table of "{(A, 1/3)  (B, 1/2)  (C, 1/12)  (D, 1/12)}"
+    symbols = {"A": 1 / 3.0, "B": 1 / 2.0, "C": 1 / 12.0, "D": 1 / 12.0}
